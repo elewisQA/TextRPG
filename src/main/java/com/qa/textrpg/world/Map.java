8aef48a5514e2f8e945488b47c4bad1561c71cec
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-16 10:30:14 +0100

    Updated world-grid and related methods.

--- src/main/java/com/qa/textrpg/world/Map.java
+++ src/main/java/com/qa/textrpg/world/Map.java
@@ -1,5 +1,6 @@
 package com.qa.textrpg.world;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.qa.textrpg.world.entities.MapObject;
@@ -7,18 +8,28 @@ import com.qa.textrpg.world.entities.MapObject;
 //===[ Map Definition ]===
 public class Map {
 	//-[ Map Variables ]-
-	private List<List<MapObject>> worldGrid;
+	private List<List<List<MapObject>>> worldGrid; // X and Y coordinates, with list of objects that may be stacked atop-each-other
 	
 	//-[ Map Constructor ]-
 	
 	
 	//-[ Map Methods ]-
-	// Modifiers
-	public MapObject getAt(int x, int y) {
+	// Specific Coordinate Methods
+	public MapObject getAt(int x, int y, int z) {
+		return worldGrid.get(x).get(y).get(z);
+	}
+	
+	public List<MapObject> getAt(int x, int y) {
 		return worldGrid.get(x).get(y);
 	}
 	
+	public void setAt(int x, int y, int z, MapObject object) {
+		worldGrid.get(x).get(y).set(z, object);
+	}
+	
 	public void setAt(int x, int y, MapObject object) {
-		worldGrid.get(x).set(y, object);
+		List<MapObject> newObject = new ArrayList<MapObject>();
+		newObject.add(object);
+		worldGrid.get(x).set(y, newObject);
 	}
 }
