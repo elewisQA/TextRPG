3ec5ba7ef3c1fab3eacdfdebc8b709f61d407c1d
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-16 10:20:24 +0100

    Added get/set methods for specific coords.

--- src/main/java/com/qa/textrpg/world/Map.java
+++ src/main/java/com/qa/textrpg/world/Map.java
@@ -1,11 +1,24 @@
 package com.qa.textrpg.world;
 
-import java.util.ArrayList;
+import java.util.List;
 
-import com.qa.textrpg.world.entities.*;
+import com.qa.textrpg.world.entities.MapObject;
 
 //===[ Map Definition ]===
 public class Map {
-	private ArrayList<ArrayList<MapObject>> worldGrid;
+	//-[ Map Variables ]-
+	private List<List<MapObject>> worldGrid;
 	
+	//-[ Map Constructor ]-
+	
+	
+	//-[ Map Methods ]-
+	// Modifiers
+	public MapObject getAt(int x, int y) {
+		return worldGrid.get(x).get(y);
+	}
+	
+	public void setAt(int x, int y, MapObject object) {
+		worldGrid.get(x).set(y, object);
+	}
 }
